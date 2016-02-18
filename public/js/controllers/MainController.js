db114804b19e5eeee03fad76e68344bf09538fca
MAlexj <alexmaximovj@gmail.com>
2016-02-18 16:44:17 +0200

    add new directive appInfo

--- public/js/controllers/MainController.js
+++ public/js/controllers/MainController.js
@@ -8,15 +8,53 @@ app.controller('MainController',
                     name: 'The Book of Trees',
                     price: 19,
                     pubdate: new Date('2014', '03', '08'),
-                    cover: 'img/index.jpeg'
+                    cover: 'img/the-book-of-trees.jpg',
+                    likes: 0,
+                    dislikes: 0
                 },
                 {
                     name: 'Program or be Programmed',
                     price: 8,
                     pubdate: new Date('2013', '08', '01'),
-                    cover: 'img/index.jpeg'
+                    cover: 'img/index.jpeg',
+                    likes: 0,
+                    dislikes: 0
                 }
-            ]
+            ];
+
+            $scope.plusOne = function (index) {
+                $scope.products[index].likes += 1;
+            };
+
+            $scope.minusOne= function(index){
+                $scope.products[index].dislikes +=1;
+            };
+
+
+            $scope.move = {
+                icon: 'img/move.jpg',
+                title: 'MOVE',
+                developer: 'MOVE, Inc.',
+                price: 0.99
+            };
+
+            $scope.shutterbugg = {
+                icon: 'img/shutterbugg.jpg',
+                title: 'Shutterbugg',
+                developer: 'Chico Dusty',
+                price: 2.99
+            };
+
+            $scope.gameboard = {
+                icon: 'img/gameboard.jpg',
+                title: 'Gameboard',
+                developer: 'Armando P.',
+                price: 1.99
+            };
+
+
+
+
         }
     ]
 );
